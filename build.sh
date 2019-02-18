c905bda9d098db582107da615906664cd4ed6e8e
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-18 18:06:25 -0500

    Update (squash once working) [ci skip]

--- build.sh
+++ build.sh
@@ -4,22 +4,11 @@ IFS=$'\n\t'
 
 # Debian + Octave
 DOCKER_REPO_BASE="octave"
-IMG_TAG=debian-latest
-
-docker build -f Dockerfile.debian -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
+for IMG_TAG in debian alpine ; do
+docker build -f Dockerfile.${IMG_TAG} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
 docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
-
-# Alpine + Octave
-IMG_TAG=alpine-latest
-
-docker build -f Dockerfile.alpine -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+done
 
 # Alpine + Octave (Source)
 # Use Docker Hub's infrastructure
 #curl -X POST -L https://cloud.docker.com/api/build/v1/source/4813e645-4451-4874-b8d4-88e787c41597/trigger/d09a812d-6a8f-4176-bf23-19141ed9e24e/call/
-
-# # To build locally, run
-#IMG_TAG=alpine-source-latest
-#docker build -f Dockerfile.alpine-source -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
