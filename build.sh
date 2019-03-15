cd98d51c612a35ba156f345a67de7ed5de208d1c
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-03-15 07:29:40 -0400

    Build GUI version on Docker Hub only

--- build.sh
+++ build.sh
@@ -18,16 +18,16 @@ docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
 
 
 # Add alternative entrypoint for mkoctfile
-IMG_TARGET=mkoctfile
-docker build -f Dockerfile.debian --target ${IMG_TARGET} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}-${IMG_TARGET} .
-docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}-${IMG_TARGET}
+#IMG_TARGET=mkoctfile
+#docker build -f Dockerfile.debian --target ${IMG_TARGET} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}-${IMG_TARGET} .
+#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}-${IMG_TARGET}
 
-IMG_TAG=gui
+## Add GUI build
+#IMG_TAG=gui
 #docker build -f Dockerfile.${IMG_TAG} --target=builder -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
 
-docker build -f Dockerfile.${IMG_TAG} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+#docker build -f Dockerfile.${IMG_TAG} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
+#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
 
-# Alpine + Octave (Source)
-# Use Docker Hub's infrastructure
-#curl -X POST -L https://cloud.docker.com/api/build/v1/source/4813e645-4451-4874-b8d4-88e787c41597/trigger/d09a812d-6a8f-4176-bf23-19141ed9e24e/call/
+# GUI build on Docker Hub's infrastructure.
+curl -X POST -L https://cloud.docker.com/api/build/v1/source/ecd4368d-cffd-4145-9eff-b104ec7f697b/trigger/28c2a1c1-a191-482e-b1fb-c06290d6fc7f/call/
