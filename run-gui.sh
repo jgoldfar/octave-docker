9d138de4422823c172343a1e066a215f2119ac1a
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-03-16 09:58:52 -0400

    Add default qt-settings file, update Qt to version 5

--- run-gui.sh
+++ run-gui.sh
@@ -12,28 +12,27 @@ fi
 
 # Find IP of local machine
 #IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-IP=$(ifconfig lo0 | grep inet | awk '$1=="inet" {print $2}')
+#IP=$(ifconfig lo0 | grep inet | awk '$1=="inet" {print $2}')
 
 # XAUTH directory
-XAUTH=/tmp/.docker.xauth
+#XAUTH=/tmp/.docker.xauth
 
-chmod 755 $XAUTH
+#chmod 755 $XAUTH
 
-xauth nlist ${DISPLAY} | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+#xauth nlist ${DISPLAY} | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 # Debian + Octave
 DOCKER_USERNAME=jgoldfar
 DOCKER_REPO_BASE=octave
 IMG_TAG=gui
 
-xhost +
+xhost +local:
 
 docker run \
        --rm \
        --tty \
        --interactive \
        --name octave \
-       --user $(id -u):$(id -g) \
        -e DISPLAY=host.docker.internal:0 \
        -e QT_GRAPHICSSYSTEM="native" \
        --workdir=/data \
@@ -41,9 +40,4 @@ docker run \
        --entrypoint="" \
        --privileged \
        ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} \
-       octave --gui --traditional --verbose --debug
-       
-       #-e XAUTHORITY=${XAUTH} \
-       #--volume ${XAUTH}:${XAUTH} \
-       #--volume ${XSOCK}:${XSOCK} \
-# --net=host \
+       octave --gui --traditional
